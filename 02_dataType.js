d4bca9a6d6c7924d44c7988e09566a23311bad8f
Ganesh <ganeshrajekshirsagar@gmail.com>
2022-10-08 21:43:11 +0530

    assignment of dataTypes
    a variable wich typeOfData (e.g string, number, boolean and number) store show by using typeof Oprator.

---
+++ 02_dataType.js
@@ -0,0 +1,9 @@
+// Q-5 create a variables for each data type like number, string, boolean also check data type 
+function typeOfData(num,str,unknown){
+}
+typeOfData(10,"Ganesh");
+console.log("Data type of 10 -> " + typeof 10);
+console.log("Data type of Ganesh -> " + typeof "Ganesh");
+var isMarried=true;
+console.log("Data type of isMarried -> " + typeof isMarried);
+console.log("Data type of not called variable -> " + typeof unknown);
