e8ff5153dd4a2b550128d8a5d7df4cf3b34f720d
Ganesh <ganeshrajekshirsagar@gmail.com>
2022-10-08 21:38:48 +0530

    Variable declaration
    using var keyword we can declare the variables

---
+++ 01_variable_assignment.js
@@ -0,0 +1,21 @@
+// Declare a variable with name --> myFullName
+var myFullName;
+console.log(myFullName);
+myFullName= "Ganesh Kshirsagar";
+console.log(myFullName);
+myFullName="Ganesh Hanumant Kshirsagar";
+console.log(myFullName);
+//Declare and Initialize a variable with name 'myLove' with value --> I love only JS
+var myLove="I love only JS";
+console.log(myLove);
+//Create a variable for form controals as listed below. Declare and initialize must be on same line.
+var firstName= "Ganesh";
+console.log(firstName);
+var lastName="Kshirsagar";
+console.log(lastName);
+var age=25;
+console.log(age);
+var address= "At/post-Ozewadi, Tal-Pandharpur, Dist-Solapur, Pin-413304";
+console.log(address);
+var gender="Male";
+console.log(gender);
