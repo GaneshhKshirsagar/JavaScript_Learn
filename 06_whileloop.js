6f5afcc636185532a352b07fb4018244e9efe6c7
Ganesh <ganeshrajekshirsagar@gmail.com>
2022-10-17 04:36:01 +0530

    While loop
    While loop

---
+++ 06_whileloop.js
@@ -0,0 +1,41 @@
+// WAP print 5 to 15 number incremented by 1
+var num=5;
+while(num<=15)
+{
+    console.log(num);
+    num++;
+}
+
+// WAP print 50 to 40 number decremented by 1
+console.log(" WAP print 50 to 40 number decremented by 1");
+
+var num=50;
+while(num>=40)
+{
+    console.log(num);
+    num--;
+}
+
+// WAP to find first 15 odd numbers
+
+console.log("WAP to find first 15 odd numbers");
+var num= 1;
+while (num<=15) {
+    if(num%2!=0)
+    {
+        console.log(num);
+    } 
+    num++;   
+}
+
+// WAP to find first 15 even numbers
+
+console.log("WAP to find first 15 even numbers");
+var num= 1;
+while (num<=15) {
+    if(num%2==0)
+    {
+        console.log(num);
+    } 
+    num++;   
+}
