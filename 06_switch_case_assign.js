144a36fe78666cf443d10e7110391437efd46650
Ganesh <ganeshrajekshirsagar@gmail.com>
2022-10-17 04:12:27 +0530

    Create 06_switch_case_assign.js
    by giving input display the appropriate month.

---
+++ 06_switch_case_assign.js
@@ -0,0 +1,88 @@
+
+// Display january, february
+
+var dismonth=1;
+
+switch (dismonth) {
+    case 1:
+        console.log("January");
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+    default:
+        break;
+}
+
+// Display december
+
+var dismonth=12;
+
+switch (dismonth) {
+    case 1:
+        console.log("January");
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+    default:
+        break;
+}
